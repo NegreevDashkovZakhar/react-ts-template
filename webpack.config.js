2ae38cf97b51b30968a6ce3ccc6e3f83bb17976d
Negreev-Dashkov Zakhar <negreevdashkov.zakhar@gmail.com>
2023-07-17 20:27:16 +0600

    feat: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+let mode = "development";
+let target = "web";
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  target = "browserslist";
+}
+
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: "./src/index.html",
+  }),
+  new MiniCssExtractPlugin({
+    filename: "[name].[contenthash].css",
+  }),
+];
+
+if (process.env.SERVE) {
+  plugins.push(new ReactRefreshWebpackPlugin());
+}
+
+module.exports = {
+  mode,
+  plugins,
+  target,
+  entry: "./src/index.js",
+
+  devtool: "source-map",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+
+  devServer: {
+    hot: true,
+  },
+
+  module: {
+    rules: [
+      { test: /\.(html)$/, use: ["html-loader"] },
+      {
+        test: /\.(s[ac]|c)ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+        type: mode === "production" ? "asset" : "asset/resource",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+};
